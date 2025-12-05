3dc171a5986cc8f96317bb004710696a15335512
Vishwanathms <vishwanath.murthy@gmail.com>
2025-12-05 11:53:18 +0530

    D4 docs

--- D3/ec2-create.sh
+++ D3/ec2-create.sh
@@ -6,11 +6,10 @@
 # 3. ec2 image id
 # 4. ec2 key_name
 # 5. ec2 name value
-SUBNET_ID=$(aws ec2 describe-subnets --filters "Name=tag:Name,Values=32-sub1" --query "Subnets[0].SubnetId" \
+SUBNET_ID=$(aws ec2 describe-subnets --filters "Name=tag:Name,Values=vishwa_sub01" --query "Subnets[0].SubnetId" \
   --output text)
 
-SG_ID=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=32-SSH-ICMP" --query "SecurityGroups[0].GroupId" \
+SG_ID=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=vishwa-sg01" --query "SecurityGroups[0].GroupId" \
   --output text)
 
-aws ec2 run-instances --image-id ami-0169aa51f6faf20d5 --count 1 --instance-type t3.micro   --key-name skv_key1 \
-  --subnet-id $SUBNET_ID --security-group-ids $SG_ID --associate-public-ip-address --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=32-vm02}]'
+aws ec2 run-instances --image-id ami-0fa3fe0fa7920f68e --count 1 --instance-type t3.micro   --key-name vishwa-key01 --subnet-id $SUBNET_ID --security-group-ids $SG_ID --associate-public-ip-address --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=31-vm01}]'
